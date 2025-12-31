41927afd188f5b7501961caee221765c88160ec6
pgp <admin@pgpkey.net>
2025-12-30 18:00:54 -0600

    need to adjust skybox & shader

---
+++ test.c
@@ -0,0 +1,188 @@
+#include "raylib.h"
+#include "rcamera.h"
+#include "rlgl.h"
+#include "raymath.h"
+#define MAX_COLUMNS 20
+
+#if defined(PLATFORM_DESKTOP)
+    #define GLSL_VERSION 330
+#else
+    #define GLSL_VERSION 100
+#endif
+
+Model skybox = { 0 };
+Shader shdrCubemap = { 0 };
+char skyboxFilename[256] = { 0 };
+bool useHDR = false;
+
+Shader pixelShader = { 0 };
+RenderTexture2D target = { 0 };
+
+
+static void DrawCrosshair(int screenWidth, int screenHeight)
+{
+    int cx = screenWidth / 2;
+    int cy = screenHeight / 2;
+
+    int size  = 10;
+    int gap   = 4;
+    int thick = 2;
+
+    Color color = RAYWHITE;
+
+    // horizontal
+    DrawRectangle(cx - size - gap, cy - thick/2, size, thick, color);
+    DrawRectangle(cx + gap,        cy - thick/2, size, thick, color);
+
+    // vertical
+    DrawRectangle(cx - thick/2, cy - size - gap, thick, size, color);
+    DrawRectangle(cx - thick/2, cy + gap,        thick, size, color);
+}
+
+
+
+int main(void)
+{
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
+
+    InitWindow(screenWidth, screenHeight, "camera test");
+    target = LoadRenderTexture(screenWidth, screenHeight);
+    SetTextureFilter(target.texture, TEXTURE_FILTER_POINT);
+
+    pixelShader = LoadShader(
+    0,
+    TextFormat("resources/shaders/glsl%i/pixelizer.fs", GLSL_VERSION)
+    );
+
+    Camera camera = { 0 };
+    camera.position = (Vector3){ 0.0f, 2.0f, 4.0f };
+    camera.target = (Vector3){ 0.0f, 2.0f, 0.0f };
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camera.fovy = 60.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    int cameraMode = CAMERA_FIRST_PERSON;
+
+    float heights[MAX_COLUMNS] = { 0 };
+    Vector3 positions[MAX_COLUMNS] = { 0 };
+    Color colors[MAX_COLUMNS] = { 0 };
+
+    for (int i = 0; i < MAX_COLUMNS; i++)
+    {
+        heights[i] = (float)GetRandomValue(1, 12);
+        positions[i] = (Vector3){ (float)GetRandomValue(-15, 15), heights[i]/2.0f, (float)GetRandomValue(-15, 15) };
+        colors[i] = (Color){ GetRandomValue(20, 255), GetRandomValue(10, 55), 30, 255 };
+    }
+
+    DisableCursor();
+
+    SetTargetFPS(120);
+
+    while (!WindowShouldClose())
+    {
+        if (IsKeyPressed(KEY_ONE))
+        {
+            cameraMode = CAMERA_FREE;
+            camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+        }
+
+        if (IsKeyPressed(KEY_TWO))
+        {
+            cameraMode = CAMERA_FIRST_PERSON;
+            camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+        }
+
+        if (IsKeyPressed(KEY_THREE))
+        {
+            cameraMode = CAMERA_THIRD_PERSON;
+            camera.up = (Vector3){ 0.0f, 1.0f, 0.0f }; 
+        }
+
+        if (IsKeyPressed(KEY_FOUR))
+        {
+            cameraMode = CAMERA_ORBITAL;
+            camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+        }
+
+        if (IsKeyPressed(KEY_P))
+        {
+            if (camera.projection == CAMERA_PERSPECTIVE)
+            {
+                cameraMode = CAMERA_THIRD_PERSON;
+                camera.position = (Vector3){ 0.0f, 2.0f, -100.0f };
+                camera.target = (Vector3){ 0.0f, 2.0f, 0.0f };
+                camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+                camera.projection = CAMERA_ORTHOGRAPHIC;
+                camera.fovy = 20.0f; 
+                CameraYaw(&camera, -135*DEG2RAD, true);
+                CameraPitch(&camera, -45*DEG2RAD, true, true, false);
+            }
+            else if (camera.projection == CAMERA_ORTHOGRAPHIC)
+            {
+                cameraMode = CAMERA_THIRD_PERSON;
+                camera.position = (Vector3){ 0.0f, 2.0f, 10.0f };
+                camera.target = (Vector3){ 0.0f, 2.0f, 0.0f };
+                camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+                camera.projection = CAMERA_PERSPECTIVE;
+                camera.fovy = 60.0f;
+            }
+        }
+
+
+        UpdateCamera(&camera, cameraMode);
+	
+        BeginTextureMode(target);
+
+            ClearBackground(BLACK);
+
+            BeginMode3D(camera);
+
+
+                DrawPlane((Vector3){ 0.0f, 0.0f, 0.0f }, (Vector2){ 32.0f, 32.0f }, RED); 
+                DrawCube((Vector3){ -16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, BLACK);
+                DrawCube((Vector3){ 16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, BLACK);
+                DrawCube((Vector3){ 0.0f, 2.5f, 16.0f }, 32.0f, 5.0f, 1.0f, BLACK);
+
+                for (int i = 0; i < MAX_COLUMNS; i++)
+                {
+                    DrawCube(positions[i], 2.0f, heights[i], 2.0f, colors[i]);
+                    DrawCubeWires(positions[i], 2.0f, heights[i], 2.0f, MAROON);
+                }
+
+                if (cameraMode == CAMERA_THIRD_PERSON)
+                {
+                    DrawCube(camera.target, 0.5f, 0.5f, 0.5f, PURPLE);
+                    DrawCubeWires(camera.target, 0.5f, 0.5f, 0.5f, DARKPURPLE);
+                }
+
+            EndMode3D();
+        EndTextureMode();
+
+            
+
+           BeginDrawing();
+                ClearBackground(BLACK);
+
+                BeginShaderMode(pixelShader);
+                  DrawTextureRec(
+                      target.texture,
+                      (Rectangle){ 0, 0, (float)target.texture.width, -(float)target.texture.height },
+                      (Vector2){ 0, 0 },
+                      WHITE
+                  );
+
+          EndShaderMode();
+
+          DrawCrosshair(screenWidth, screenHeight);
+
+          
+
+        EndDrawing();
+    }
+
+    CloseWindow(); 
+
+    return 0;
+}
+
