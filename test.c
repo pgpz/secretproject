18a3c651c282ac67e5cb026675d7b8832bd70daf
pgp <admin@pgpkey.net>
2026-01-06 13:54:29 -0600

    Update test.c

--- test.c
+++ test.c
@@ -59,7 +59,7 @@ int main(void)
     camera.position = (Vector3){ 0.0f, 2.0f, 4.0f };
     camera.target = (Vector3){ 0.0f, 2.0f, 0.0f };
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-    camera.fovy = 60.0f;
+    camera.fovy = 90.0f;
     camera.projection = CAMERA_PERSPECTIVE;
 
     int cameraMode = CAMERA_FIRST_PERSON;
